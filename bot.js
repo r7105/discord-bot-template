184f3e91e4aa94f9207d78a5fb29823028ae7a9d
SMg <139099690+speedevil50@users.noreply.github.com>
2025-05-01 08:01:54 -0400

    Update bot.js
    added extensive descriptions to lines
    
    Signed-off-by: SMg <139099690+speedevil50@users.noreply.github.com>

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config(); // To use environment variables
+const { Client, GatewayIntentBits } = require('discord.js'); // Imports the discord.js system
+require('dotenv').config(); // To use environment variables (this is entirely optional but if you remove this line replace (wrapped with >><<) `client.login(>>process.env.DISCORD_TOKEN<<)` with your token)
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }); // these are generally required, do not remove this
 
 // Ready event
 client.once('ready', () => {
@@ -10,15 +10,15 @@ client.once('ready', () => {
 });
 
 // Message listener
-client.on('messageCreate', (message) => {
+client.on('messageCreate', (message) => { // this will run when the bot detects a message sent
     // Ignore messages from the bot itself
-    if (message.author.bot) return;
+    if (message.author.bot) return; // this adds a check for if the user is a bot, if the user is a bot then this should return true `(message.author.>>bot<<)` if not then it will return false and continue to the next line
 
     // Check for a specific command
-    if (message.content === '!ping') {
-        message.reply('Pong!');
+    if (message.content === '!ping') { // when a member sends a message containing '!ping' (activated by `if (message.content === ...) 
+        message.reply('Pong!'); // sends a message as the bot
     }
 });
 
 // Login to Discord with your app's token
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN); // runs the bot
